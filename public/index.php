636791aa29582a725e2cc40d6756f0d326adcc39
nick <nick.x.a.k@gmail.com>
2019-05-27 17:17:16 +0300

    Lesson 1 part 1

--- public/index.php
+++ public/index.php
@@ -1,13 +1,20 @@
 <?php
 use Framework\Http\Request;
+use Framework\Http\RequestFactory;
+use Framework\Http\Response;
 require __dir__ . "/../vendor/autoload.php";
 
 ### Initialization
 
-$request = new Request($_GET, $_POST);
+$request = RequestFactory::fromGlobals();
 
 ### Action
 
 $name = $request->getQueryParams()['name'] ?? 'Guest';
-header('X-Developer: Nick');
-echo 'Hello, ' . $name . '!';
+$response = (new Response('Hello, ' . $name . '!'))
+	->withHeader('X-Developer', 'Nick');
+header('HTTP/1.1 ' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
+foreach ($response->getHeaders() as $name => $value) {
+	header($name . ':' . $value);
+}
+echo $response->getBody();
--- src/Framework/Http/Request.php
+++ src/Framework/Http/Request.php
@@ -1,24 +1,115 @@
 <?php
 namespace Framework\Http;
 
-class Request
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UriInterface;
+
+class Request implements ServerRequestInterface
 {
-	private $queryParams;
+	private $queryParams = [];
 	private $parseBody;
 
-	public function __construct(array $queryParams = [], $parseBody = null)
+	/*public function __construct(array $queryParams = [], $parseBody = null)
 	{
 		$this->queryParams = $queryParams;
 		$this->parseBody = $parseBody;
-	}
+	}*/
 
 	public function getQueryParams(): array
 	{
 		return $this->queryParams;
 	}
 
+	public function withQueryParams(array $query): self
+	{
+		$new = clone $this;
+		$new->queryParams = $query;
+		return $new;
+	}
+
 	public function getParseBody()
 	{
 		return $this->parseBody ?: null;
 	}
+
+	public function withParseBody($data): self
+	{
+		$new = clone $this;
+		$new->parseBody = $data;
+		return $new;
+	}
+	public function getAttribute( $name, $default = null ) {}
+	public function getAttributes() {}
+	public function getBody() {}
+	public function getCookieParams() {}
+	public function getHeader( $name ) {}
+	public function getHeaderLine( $name ) {}
+	public function getHeaders() {
+		// TODO: Implement getHeaders() method.
+	}
+	public function getMethod() {
+		// TODO: Implement getMethod() method.
+	}
+	public function getParsedBody() {
+		// TODO: Implement getParsedBody() method.
+	}
+	public function getProtocolVersion() {
+		// TODO: Implement getProtocolVersion() method.
+	}
+	public function getRequestTarget() {
+		// TODO: Implement getRequestTarget() method.
+	}
+	public function getServerParams() {
+		// TODO: Implement getServerParams() method.
+	}
+	public function getUploadedFiles() {
+		// TODO: Implement getUploadedFiles() method.
+	}
+	public function getUri() {
+		// TODO: Implement getUri() method.
+	}
+	public function hasHeader( $name ) {
+		// TODO: Implement hasHeader() method.
+	}
+	public function withAddedHeader( $name, $value ) {
+		// TODO: Implement withAddedHeader() method.
+	}
+	public function withAttribute( $name, $value ) {
+		// TODO: Implement withAttribute() method.
+	}
+	public function withBody( StreamInterface $body ) {
+		// TODO: Implement withBody() method.
+	}
+	public function withCookieParams( array $cookies ) {
+		// TODO: Implement withCookieParams() method.
+	}
+	public function withHeader( $name, $value ) {
+		// TODO: Implement withHeader() method.
+	}
+	public function withMethod( $method ) {
+		// TODO: Implement withMethod() method.
+	}
+	public function withoutAttribute( $name ) {
+		// TODO: Implement withoutAttribute() method.
+	}
+	public function withoutHeader( $name ) {
+		// TODO: Implement withoutHeader() method.
+	}
+	public function withParsedBody( $data ) {
+		// TODO: Implement withParsedBody() method.
+	}
+	public function withProtocolVersion( $version ) {
+		// TODO: Implement withProtocolVersion() method.
+	}
+	public function withRequestTarget( $requestTarget ) {
+		// TODO: Implement withRequestTarget() method.
+	}
+	public function withUploadedFiles( array $uploadedFiles ) {
+		// TODO: Implement withUploadedFiles() method.
+	}
+	public function withUri( UriInterface $uri, $preserveHost = false ) {
+		// TODO: Implement withUri() method.
+	}
+
 }
